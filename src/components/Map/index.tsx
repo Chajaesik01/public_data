1238d50eab4788c01b59e82e3887a4c36dcd38f5
chajaesik01 <ckwotlr@dongguk.edu>
2024-12-07 21:58:37 +0900

    Fix: URL

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -61,7 +61,8 @@ const Map: React.FC<{ address?: string }> = ({ address }) => {
 
     const script = document.createElement('script');
     //script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAPS_API_KEY}&libraries=services`;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VITE_KAKAO_MAPS_API_KEY}&libraries=services`;
+    //script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VITE_KAKAO_MAPS_API_KEY}&libraries=services`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAPS_API_KEY}&libraries=services`;
     script.async = true;
     script.onload = getCurrentPosition;
     document.head.appendChild(script);
